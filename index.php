aae4f15d7c0664954c417d137d3e16dc4e45577f
Alexandre SANDOLO <asandolo@student.42.fr>
2018-07-03 13:12:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,46 @@
 <?php
 // My microservice!
-if(!defined('STDOUT')) define('STDOUT', fopen('php://stdout', 'w'));
-fwrite(STDOUT, 'Fighter is running');
+for ($i = 4; $i <= 9; $i++)
+{
+	if(!defined('STDOUT')) define('STDOUT', fopen('php://stdout', 'w'));
+	fwrite(STDOUT, 'Fighter is running');
 
-set_exception_handler(function ($e) {
-    $code = $e->getCode() ? : 400;
-    header("Content-Type: application/json", NULL, $code);
-    echo json_encode(["error" => $e->getMessage()]);
-    exit;
-});
+	set_exception_handler(function ($e) {
+	    $code = $e->getCode() ? : 400;
+	    header("Content-Type: application/json", NULL, $code);
+	    echo json_encode(["error" => $e->getMessage()]);
+	    exit;
+	});
 
-// assume JSON, handle requests by verb and path
-$verb = $_SERVER['REQUEST_METHOD'];
+	// assume JSON, handle requests by verb and path
+	$verb = $_SERVER['REQUEST_METHOD'];
 
-// declare GET request response and database query response
-$getRequestResponse;
+	// declare GET request response and database query response
+	$getRequestResponse;
 
-error_log(print_r($verb, TRUE));
-switch ($verb) {
-    case 'GET':
-    	// sample GET URL
-        //$url = "http://ip.jsontest.com/";
-	//$url = "http://129.157.179.180:3000/shield/33/45/red/asandolo";
-	$url = "http://129.157.179.180:3000/fighters/45/3/red/asandolo";
-	$getRequestResponse = file_get_contents($url);
-					
-        break;
-    default:
-        throw new Exception('Method Not Supported', 405);
+	error_log(print_r($verb, TRUE));
+	switch ($verb) {
+	    case 'GET':
+		// sample GET URL
+		//$url = "http://ip.jsontest.com/";
+		//$url = "http://129.157.179.180:3000/shield/33/45/red/asandolo";
+
+		$url = "http://129.157.179.180:3000/fighters/45/".$i."/red/asandolo";
+		$getRequestResponse = file_get_contents($url);
+
+
+		break;
+	    default:
+		throw new Exception('Method Not Supported', 405);
+	}
+
+	header("Content-Type: application/json");
+	header('Access-Control-Allow-Origin:*');
+	header('Access-Control-Allow-Methods:GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	header('Access-Control-Allow-Headers:X-Requested-With,content-type');
+	header('Access-Control-Allow-Credentials:true');
+
+	echo $getRequestResponse;
+	fwrite(STDOUT, $getRequestResponse);
+	fclose(STDOUT);
 }
-
-header("Content-Type: application/json");
-header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:GET, POST, OPTIONS, PUT, PATCH, DELETE');
-header('Access-Control-Allow-Headers:X-Requested-With,content-type');
-header('Access-Control-Allow-Credentials:true');
-
-echo $getRequestResponse;
-fwrite(STDOUT, $getRequestResponse);
-fclose(STDOUT);
