e4d2c19fb96004285c18a698ae41cd367989455f
Alexandre SANDOLO <asandolo@student.42.fr>
2018-07-03 12:46:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ switch ($verb) {
     case 'GET':
     	// sample GET URL
         //$url = "http://ip.jsontest.com/";
-	$url = "http://129.157.179.180:3000/shield/33/45/red/Asandolo";
+	$url = "http://129.157.179.180:3000/shield/33/45/red/asandolo";
 	$getRequestResponse = file_get_contents($url);
 					
         break;
